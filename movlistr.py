02c21fbb50389539cdb689a0540314166599ab3a
Daniel Bordak <dbordak@gmail.com>
2013-09-29 10:06:30 -0400

    How about this?

--- movlistr.py
+++ movlistr.py
@@ -138,7 +138,7 @@ def searchRoute(group):
 	#return str(request.form['data'])
 	resultJson = search(group, request.form['data'])
 	results2 = makeResultJson(resultJson)
-	return str(jsonToArray(results2)).replace("u'","'").replace('u"',"'").replace('"',"'")
+	return str(jsonToArray(results2)).replace("u'","'").replace('u"','"')
 
 
 if __name__ == "__main__":
